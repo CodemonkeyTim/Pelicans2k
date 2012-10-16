d8febe93678f25b3d710a7be14cea0d0181216bf
CodemonkeyTim <tim@silvertreksystems.com>
2012-10-16 15:51:11 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pelicans2k::Application.config.secret_token = '1c3a13fe0b6a4e50370386121feed3592b2b6f7ae334be5925ae2e56dcc0cf43ac45b69449d1c19ae6d39de14668aa3ce1c2d6be67e7b836c33c4fd8e3042b97'
