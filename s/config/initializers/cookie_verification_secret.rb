d276fc012c680b6ad89a1b9d9abc64547699809e
CodemonkeyTim <tim@silvertreksystems.com>
2012-11-27 02:45:59 -0800

    Creates manage_players and manage_staffs pages

---
+++ s/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '29e7ef278c7b64ba23bf550688e46c95fafe3deb6f3f57beb8f49defdc0132245a22b720a9e5c1770c44f38ecbce77d9d10f6f8eb2e24e0a4b9c8fbb6ca7ddba';
