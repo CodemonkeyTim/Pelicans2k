9bf8a130dbda685d8fdf1b298ddca44d4e0d6508
Teemu Vainio <tim@silvertreksystems.com>
2012-11-28 03:03:39 +0200

    Adds some styling and javascript plus controller code to main page calendar management page + other

--- app/controllers/private_pages_controller.rb
+++ app/controllers/private_pages_controller.rb
@@ -1,6 +1,8 @@
 class PrivatePagesController < ApplicationController
   layout "private"
   
+  @sid = 86400
+  
   def login
     render :layout => nil
   end
@@ -13,6 +15,9 @@ class PrivatePagesController < ApplicationController
   end
   
   def manage_main_calendar
+    @days = ["Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai", "Sunnuntai"]
+    @year = Time.now.strftime("%Y")
+    
     
   end
   
