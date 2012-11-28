c6f860a5b52e663563dfdb81c19b9d593ee5a30b
CodemonkeyTim <tim@silvertreksystems.com>
2012-11-28 04:56:01 -0800

    Work work...

--- app/controllers/private_pages_controller.rb
+++ app/controllers/private_pages_controller.rb
@@ -18,7 +18,24 @@ class PrivatePagesController < ApplicationController
     @days = ["Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai", "Sunnuntai"]
     @year = Time.now.strftime("%Y")
     
+    today = DateTime.now
+    mon = today.at_beginning_of_week
     
+    tue, wed, thu, fri, sat, sun = 0
+    
+    dates = [mon, tue, wed, thu, fri, sat, sun]
+    
+    dates.each_with_index do |date, i|
+      unless i == 6
+        dates[i+1] = date.tomorrow
+      end
+    end
+    
+    @date_strs = []
+    
+    dates.each do |date|
+      @date_strs.push(date.strftime("%d.%m"))
+    end  
   end
   
   def manage_players
