0c141a2540e1350da5c80b0103ded7c1cb30fc8b
CodemonkeyTim <tim@silvertreksystems.com>
2012-12-08 00:19:14 -0800

    REsolves merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Pelicans2k::Application.routes.draw do
   match "/tiimikalenterin_hallinta" => "private_pages#manage_team_calendar"
   match "/muokkaa_uutista/:id" => "private_pages#update_news"
   match "/muokkaa_pelajaat/:id" => "private_pages#edit_players" 
-  match "/manage_players" => "private_pages#manage_players"
+  match "/pelaajien_hallinta" => "private_pages#manage_players"
 
   #Ajax calls routing
   match "/get_cal_for_team" => "ajax#get_cal_for_team"
