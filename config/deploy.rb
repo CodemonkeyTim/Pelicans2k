c86990377154200b0f6cfc76b50dd551ace28ca1
CodemonkeyTim <tim@silvertreksystems.com>
2013-02-20 09:02:06 -0800

    Adds deploy setup to deploy locally

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,16 +7,24 @@ set :branch, "master"
 
 ssh_options[:forward_agent] = true
 
-set :user, "root"  # The server's user for deploys
-set :scm_passphrase, "Jd386Jma354y"  # The deploy user's password
+#set :user, "root"  # The server's user for deploys
+#set :scm_passphrase, "Jd386Jma354y"  # The deploy user's password
+set :user, "teme"  # The server's user for deploys
+set :scm_passphrase, "Tvaini0"  # The deploy user's password
 
-set :deploy_to, "/root/rails"
+#set :deploy_to, "/root/rails"
+set :deploy_to, "/home/teme/rails/deployments"
 
 default_run_options[:pty] = true
 
-role :web, "178.251.154.93"                          # Your HTTP server, Apache/etc
-role :app, "178.251.154.93"                          # This may be the same as your `Web` server
-role :db,  "178.251.154.93", :primary => true        # This is where Rails migrations will run
+#role :web, "178.251.154.93"                          # Your HTTP server, Apache/etc
+#role :app, "178.251.154.93"                          # This may be the same as your `Web` server
+#role :db,  "178.251.154.93", :primary => true        # This is where Rails migrations will run
+
+role :web, "pelicans2k.ddns.net"                          # Your HTTP server, Apache/etc
+role :app, "pelicans2k.ddns.net"                          # This may be the same as your `Web` server
+role :db,  "pelicans2k.ddns.net", :primary => true        # This is where Rails migrations will run
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
