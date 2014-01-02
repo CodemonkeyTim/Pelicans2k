5b5dd44bc337256fa13ea3749b6d44c04c650099
CodemonkeyTim <tim@silvertreksystems.com>
2014-01-02 09:34:33 +0200

    Final teamname problem fix

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -38,7 +38,13 @@ class AjaxController < ApplicationController
       if res.activity_type == "game"
         resses_json.push({id: res.id, display_activity: res.display_activity, starts_at: res.starts_at, date: res.date, activity_type: res.activity_type})
       else
-        resses_json.push({id: res.id, display_activity: res.display_activity, starts_at: res.starts_at, date: res.date, team_name: Team.find(res.team_id).name, activity_type: res.activity_type})  
+        team = Team.where(:id => res.team_id).first
+        
+        if team.nil?
+          next
+        end
+        
+        resses_json.push({id: res.id, display_activity: res.display_activity, starts_at: res.starts_at, date: res.date, team_name: team.name, activity_type: res.activity_type})  
       end
       
     end
